382b18d1fc177254c86d31ac072cd8f252497aa9
Guilherme <guilhermedev96@gmail.com>
2024-07-09 18:46:36 -0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import { ThemeProvider } from "styled-components"
+import { defaultTheme } from "./styles/themes/default"
+import { GlobalStyle } from "./styles/GlobalStyles"
+
+function App() {
+
+
+  return (
+
+    <ThemeProvider theme={defaultTheme}>
+      <GlobalStyle />
+      <h1>Hello world</h1>
+    </ThemeProvider>
+
+  )
+}
+
+export default App
